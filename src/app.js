85f74b4f889735fde6f5376cfe95a4a8a25c482a
Shashank Saxena <ssaxen4@illinois.edu>
2015-11-07 13:27:29 -0600

    finished initial release
    Finished initial release

--- src/app.js
+++ src/app.js
@@ -1,35 +1,59 @@
-var getGeoLocationURL = 'https://developer.cumtd.com/api/v2.2/json/GetStopsByLatLon?key=79279d634cac41da9258b1875237c75a&lat=40.1040557&lon=-88.2346911&count=5';
-// Get stops based on location
-// git_test
 var UI = require('ui');
 var ajax = require('ajax');
+var lat;
+var lon;
+var arrayLength;
+var locationOptions = {
+    enableHighAccuracy: true,
+    maximumAge: 10000,
+    timeout: 10000
+};
 
-var stopIDs = [];
+function locationSuccess(pos) {
+    lat = pos.coords.latitude;
+    lon = pos.coords.longitude;
+}
+
+function locationError(err) {
+    console.log('location error (' + err.code + '): ' + err.message);
+}
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+}
 
 var getStopURL = 'https://developer.cumtd.com/api/v2.2/json/GetDeparturesByStop?';
 var key = '79279d634cac41da9258b1875237c75a';
 
-var stopsNearYou = new UI.Menu({}); // Menu for showing all the stops near your current location
-var stopTimings = new UI.Menu({}); // Menu for the timings of the selected stop
+var stopIDs = [];
+var stopsNearYou = new UI.Menu({});
+var stopTimings = new UI.Menu({});
+
+var timeOfBuses = {
+    title: 'test',
+    items: [{
+        title: '',
+        subtitle: ''
+    }, {
+        title: '',
+        subtitle: ''
+    }]
+};
 
 var menuList = {
-    // List of stops near you, part of stopsNearYou
     title: 'Bus Stops Near You',
     items: [{
         title: 'ERROR',
-        //subtitle: 'ERROR'
     }, {
         title: 'ERROR',
-        //subtitle: 'ERROR'
     }, {
         title: 'ERROR',
-        //subtitle: 'ERROR'
     }, {
         title: 'ERROR',
-        //subtitle: 'ERROR'
     }, {
         title: 'ERROR',
-        //subtitle: 'ERROR'
+    }, {
+        title: 'ERROR'
     }]
 };
 
@@ -39,59 +63,23 @@ var main = new UI.Card({
     body: 'Welcome to the UIUC bus app!\n\nShashank Saxena',
     scrollable: false,
 });
+var emptyArrayCard = new UI.Card({
+  title: '     No Buses',
+  subtitle: '\n    No buses within 30 mins',
+  scrollable: false
+});
 
 main.show();
 
 main.on('click', 'select', function(e) {
-
+    getLocation();
+    var getGeoLocationURL = 'https://developer.cumtd.com/api/v2.2/json/GetStopsByLatLon?key=' + key + '&lat=' + lat + '&lon=' + lon + '&count=6';
+    // lat=40.108578
+    // lon=-88.228777
+    // This points to Transit Plaza
     if (stopIDs) {
         stopIDs = [];
     }
-
-    stopsNearYou.on('select', function(e) {
-        ajax({
-                url: getStopURL + 'key=' + key + '&stop_id=' + stopIDs[e.itemIndex],
-                type: 'json',
-                method: 'get',
-                async: false
-            },
-            function(data) {
-                console.log(e.itemIndex);
-                console.log(getStopURL + 'key=' + key + '&stop_id=' + stopIDs[e.itemIndex]);
-                if (data) {
-                    data = JSON.parse(JSON.stringify(data));
-                    console.log(JSON.stringify(data));
-                    menuList.title = e.item.title; {
-                        menuList = {
-                            // Update the SAME menu object to have the timings of the chosen stop
-                            title: e.item.title,
-                            items: [{
-                                title: data.departures[0].headsign,
-                                subtitle: data.departures[0].expected_mins + ' Minute(s)'
-                            }, {
-                                title: data.departures[1].headsign,
-                                subtitle: data.departures[1].expected_mins + ' Minute(s)'
-                            }, /*{
-                                title: data.departures[2].headsign,
-                                subtitle: data.departures[2].expected_mins + ' Minute(s)'
-                            }, {
-                                title: data.departures[3].headsign,
-                                subtitle: data.departures[3].expected_mins + ' Minute(s)'
-                            }, {
-                                title: data.departures[4].headsign,
-                                subtitle: data.departures[4].expected_mins + ' Minute(s)'*/
-                            ]
-                        };
-                    }
-                }
-                stopTimings.section(0, menuList);
-                stopTimings.show();
-            },
-            function(err, stat, req) {
-                console.log('ERROR - ' + err);
-            }
-        );
-    });
 
     ajax({
             //url: baseURL + 'key=' + key + '&stop_id=' + 'iu' + '&count=' + '5',
@@ -103,29 +91,252 @@ main.on('click', 'select', function(e) {
         function(data) {
             if (data) {
                 data = JSON.parse(JSON.stringify(data)); // JSON validator
-                for (var i = 0; i < 5; i++) {
+
+                for (var i = 0; i < 6; i++) {
                     stopIDs.push(data.stops[i].stop_id);
                     menuList.items[i].title = data.stops[i].stop_name;
                 }
-              stopIDs = stopIDs.splice('');
-/*                for (var j = 0; j < stopIDs.length; j++)
-                {
-                  if(stopIDs[j] == 'GRGIKE')
-                    {
-                      stopIDs[j] = 'GRGIKE:1';
-                    }
-                }
-*/
-              stopIDs = stopIDs.slice(0,5);
-              console.log(stopIDs);
+
+                stopIDs = stopIDs.splice('');
+                stopIDs = stopIDs.slice(0, 6);
                 stopsNearYou.section(0, menuList);
-                //console.log(section.items[0].title);
-                console.log(stopIDs);
                 stopsNearYou.show();
             }
         },
         function(err, stat, req) {
-            console.log('ERROR - ' + err);
+            console.log('ERROR - main.on - ' + err);
         }
     );
+
+    stopsNearYou.on('select', function(e) {
+        ajax({
+                url: getStopURL + 'key=' + key + '&stop_id=' + stopIDs[e.itemIndex],
+                type: 'json',
+                method: 'get',
+                async: false
+            },
+            function(data) {
+                console.log(getStopURL + 'key=' + key + '&stop_id=' + stopIDs[e.itemIndex]);
+
+                if (data) {
+                    data = JSON.parse(JSON.stringify(data)); //JSON Validator
+                    arrayLength = data.departures.length;
+                    timeOfBuses.title = e.item.title;
+                    switch (arrayLength) {
+                        case 0:
+                            emptyArrayCard.show();
+                            break;
+
+                        case 1:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 2:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 3:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 4:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[3].headsign,
+                                    subtitle: data.departures[3].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 5:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[3].headsign,
+                                    subtitle: data.departures[3].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[4].headsign,
+                                    subtitle: data.departures[4].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 6:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[3].headsign,
+                                    subtitle: data.departures[3].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[4].headsign,
+                                    subtitle: data.departures[4].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[5].headsign,
+                                    subtitle: data.departures[5].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 7:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[3].headsign,
+                                    subtitle: data.departures[3].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[4].headsign,
+                                    subtitle: data.departures[4].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[5].headsign,
+                                    subtitle: data.departures[5].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[6].headsign,
+                                    subtitle: data.departures[6].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        case 8:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[3].headsign,
+                                    subtitle: data.departures[3].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[4].headsign,
+                                    subtitle: data.departures[4].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[5].headsign,
+                                    subtitle: data.departures[5].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[6].headsign,
+                                    subtitle: data.departures[6].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[7].headsign,
+                                    subtitle: data.departures[7].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+
+                        default:
+                            timeOfBuses = {
+                                title: e.item.title,
+                                items: [{
+                                    title: data.departures[0].headsign,
+                                    subtitle: data.departures[0].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[1].headsign,
+                                    subtitle: data.departures[1].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[2].headsign,
+                                    subtitle: data.departures[2].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[3].headsign,
+                                    subtitle: data.departures[3].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[4].headsign,
+                                    subtitle: data.departures[4].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[5].headsign,
+                                    subtitle: data.departures[5].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[6].headsign,
+                                    subtitle: data.departures[6].expected_mins + ' Minute(s)'
+                                }, {
+                                    title: data.departures[7].headsign,
+                                    subtitle: data.departures[7].expected_mins + ' Minute(s)'
+                                }]
+                            };
+                            break;
+                    }
+                }
+                if (arrayLength > 0) {
+                    timeOfBuses = JSON.parse(JSON.stringify(timeOfBuses));
+
+
+                    stopTimings.section(0, timeOfBuses);
+                    stopTimings.show();
+                    timeOfBuses = {};
+                }
+
+            },
+            function(err, stat, req) {
+                console.log('ERROR - stopsNearYou.on - ' + err);
+            }
+        );
+    });
+
 });
